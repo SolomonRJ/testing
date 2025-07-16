a7fc1573424a2bf4a83473323435053d863b1dd3
SolomonRJ <solomonraja299@gmail.com>
2025-07-16 14:34:55 +0530

    Add initial project files

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// Register service worker
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
